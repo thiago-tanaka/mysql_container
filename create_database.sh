82d15cad5f69b31ebae54e8d6816354a06e5aa48
thiago-tanaka <thiago.tanaka.dev@gmail.com>
2023-11-29 16:04:43 +0900

    remove .env from script

--- create_database.sh
+++ create_database.sh
@@ -7,4 +7,4 @@ read database_name
 add_privileges_command="GRANT ALL PRIVILEGES ON $database_name.* TO 'mysql_user'@'%'; FLUSH PRIVILEGES;"
 create_database_command="CREATE DATABASE $database_name;"
 
-docker exec --env-file=.env mysql bash -c "mysql -uroot -pmysql_password -e \"$add_privileges_command $create_database_command\""
+docker exec mysql bash -c "mysql -uroot -pmysql_password -e \"$add_privileges_command $create_database_command\""
