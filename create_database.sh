e1163d73020091376672657a24302a5784ed473e
thiago-tanaka <thiago.tanaka.dev@gmail.com>
2023-11-29 15:44:27 +0900

    add create_database.sh script

---
+++ create_database.sh
@@ -0,0 +1,10 @@
+##!/bin/bash
+
+echo "Inform the database name:"
+
+read database_name
+
+add_privileges_command="GRANT ALL PRIVILEGES ON $database_name.* TO 'mysql_user'@'%'; FLUSH PRIVILEGES;"
+create_database_command="CREATE DATABASE $database_name;"
+
+docker exec --env-file=.env mysql bash -c "mysql -uroot -pmysql_password -e \"$add_privileges_command $create_database_command\""
